9755534833c1f99f5b87bdb70efacf886d25aff0
Thiago Jordão <thiagojordao98@gmail.com>
2021-12-02 23:40:40 -0300

    LIFOO project start

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { useSelector } from 'react-redux';
+
+import HomeScreen from './pages/HomeScreen';
+import Tela2Screen from './pages/Tela2Screen';
+
+export default () => {
+    const name = useSelector(state => state.user.name);
+
+    return (
+        <BrowserRouter>
+            <h1>Oi, {name}</h1>
+
+            <Switch>
+                <Route exact path="/">
+                    <HomeScreen />
+                </Route>
+                <Route path="/tela2/:nome">
+                    <Tela2Screen />
+                </Route>
+            </Switch>
+        </BrowserRouter>
+    );
+}
